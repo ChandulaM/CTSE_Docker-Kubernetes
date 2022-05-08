cc5887e9707c22f1bb681e9076b60e35cbb44a5c
P.D.G.N.T.D. Dharmasinghe <dilhannadun777@gmail.com>
2022-05-08 17:33:54 +0530

    changed directory2

--- FastLK/BuyerService/src/controller/authenticationController.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { login, validatetoken } = require("../service/authenticationService");
-
-router.post('/login', login);
-router.post('/validatetoken', validatetoken);
-
-module.exports = router;
